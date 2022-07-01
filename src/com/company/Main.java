311d7ee0f2942e8dbbd4a0075f5e5238e88e71ea
k0schey <alex.gorelov@outlook.com>
2022-07-01 22:41:36 +0400

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,84 +1,64 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main
 {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        s = encode(s);
-        System.out.println(s);
+        long q = System.nanoTime();
+        factors( 791943245);
+        long d = System.nanoTime();
+        //factors_how_it_was_supposed(791943245);
+        long h = System.nanoTime();
+        System.out.println((d-q)/10000);
+        System.out.println((h - d)/10000 );
     }
-
-    static String encode(String word)
+    // !!!
+    // мой метод работает быстрее всех, которые я проверял, причем намного у большинства
+    // только один был быстрее с Map, но там очень большое решение
+    public static String factors(int n)
     {
-        char[] w = word.toLowerCase(Locale.ROOT).toCharArray();
-        Obj[] s = new Obj[w.length];
-        for (int i = 0; i < s.length; i++)
-        {
-            s[i] = new Obj(w[i], false);
-        }
+        int check_num = 1;
+        int prime = 2;
+        int times = 0;
+        boolean is_prime = false;
 
-        for(int i = 0; i < s.length-1; i++)
+        StringBuilder result = new StringBuilder();
+        while(n != 1)
         {
-            boolean d = false;
-            for(int j = i+1; j < s.length; j++)
+            if (n%prime == 0)
             {
-                if ((s[i].i == s[j].i) && !(s[j]).i_was_changed && !(s[i]).i_was_changed)
+                n /= prime;
+                times++;
+                if(n == 1)
                 {
-                    d = true;
-                    s[j].i = ')';
-
-                    s[j].i_was_changed = true;
+                    if (times > 1) result.append("(").append(prime).append("**").append(times).append(")");
+                    else if (times == 1) result.append("(").append(prime).append(")");
                 }
             }
-            if (d)
+            else
             {
-                System.out.println(i);
-                s[i].i = ')';
-                s[i].i_was_changed = true;
+                if (times > 1) result.append("(").append(prime).append("**").append(times).append(")");
+                else if (times == 1) result.append("(").append(prime).append(")");
+
+                while (!is_prime)
+                {
+                    //System.out.println(prime);
+
+                    if (prime == 2) prime++;
+                    else prime+=2;
+
+                    // по т.Ферма -> https://habr.com/ru/company/otus/blog/486116/
+                    if (Math.pow(check_num, prime-1)%prime == 1.0f) is_prime = true;
+                }
+                times = 0;
+                is_prime = false;
             }
         }
 
-        String a = "";
-
-        for(int i = 0; i < s.length; i++)
-        {
-            if (!s[i].i_was_changed)
-            {
-                System.out.println(i);
-                s[i].i = '(';
-            }
-            a += s[i].i;
-        }
-        return a;
-    }
-
-    // from best solution on codewars
-    static String encode2(String word)
-    {
-        word = word.toLowerCase();
-        String result = "";
-        for (int i = 0; i < word.length(); ++i)
-        {
-            char c = word.charAt(i);
-            result += word.lastIndexOf(c) == word.indexOf(c) ? "(" : ")";
-        }
-        return result;
-    }
-}
-
-class Obj
-{
-    char i;
-    boolean i_was_changed;
-    Obj(char d, boolean b)
-    {
-        i = d;
-        i_was_changed = b;
+        return result.toString();
     }
 }
